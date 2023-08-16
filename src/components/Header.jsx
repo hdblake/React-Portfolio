19468ce2d9818514e4f8956318dc8c1b1ceda7d9
= <=>
2023-08-16 16:29:22 -0500

    created header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <header className="sticky top-0 bg-accent3">
+      <div className="flex flex-row items-center justify-between m-auto p-5 mb-8 w-full lg:w-3/4">
+        <img src="../images/logo.webp" alt="logo" width={190} height={59} />
+        <nav>
+          <ul className="flex flex-row">
+            <li className="mr-5 text-main text-xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+              <a href="#home">Home</a>
+            </li>
+            <li className="text-main text-xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+              <a href="#about">About</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
