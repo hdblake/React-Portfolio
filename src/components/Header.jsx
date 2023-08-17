478686522aabf6a8efe47f5ccd74a7d52eeec624
= <=>
2023-08-17 01:10:58 -0500

    updated font size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ export default function Header() {
         <img src="../images/logo.webp" alt="logo" width={190} height={59} />
         <nav>
           <ul className="flex flex-row">
-            <li className="mr-5 text-main text-xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+            <li className="mr-5 text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
               <a href="#home">Home</a>
             </li>
-            <li className="mr-5 text-main text-xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+            <li className="mr-5 text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
               <a href="#about">About</a>
             </li>
-            <li className="text-main text-xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+            <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
               <a href="#projects">Projects</a>
             </li>
           </ul>
