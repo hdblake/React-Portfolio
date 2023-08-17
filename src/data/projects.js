4352f70a4b93c3eabc9723336244f0ae968cf434
= <=>
2023-08-17 00:40:03 -0500

    created projects data

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Electrons",
+    image: "../images/electrons.png",
+    link: "https://electrons.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Chamber of Commerce",
+    image: "../images/chamber.png",
+    link: "hdblake.github.io/Chamber-of-Commerce/",
+  },
+  {
+    id: 3,
+    title: "Bountiful Foods",
+    image: "../images/bountiful.png",
+    link: "hdblake.github.io/Bountiful-Foods/",
+  },
+  {
+    id: 4,
+    title: "Sleep Outside",
+    image: "../images/sleepoutside.png",
+    link: "https://sleep-outside-t10.netlify.app/",
+  },
+];
