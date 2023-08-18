0870111ddb9465f997d9bdd1357afd86dbd70623
= <=>
2023-08-18 14:23:32 -0500

    added repository links

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,23 +4,27 @@ export const projects = [
     title: "Electrons",
     image: "../images/electrons.png",
     link: "https://electrons.vercel.app/",
+    repo: "https://github.com/hdblake/Electrons-website",
   },
   {
     id: 2,
     title: "Chamber of Commerce",
     image: "../images/chamber.png",
     link: "hdblake.github.io/Chamber-of-Commerce/",
+    repo: "https://github.com/hdblake/Chamber-of-Commerce",
   },
   {
     id: 3,
     title: "Bountiful Foods",
     image: "../images/bountiful.png",
     link: "hdblake.github.io/Bountiful-Foods/",
+    repo: "https://github.com/hdblake/Bountiful-Foods",
   },
   {
     id: 4,
     title: "Sleep Outside",
     image: "../images/sleepoutside.png",
     link: "https://sleep-outside-t10.netlify.app/",
+    repo: "https://github.com/hdblake/sleepoutside-team10",
   },
 ];
