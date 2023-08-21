cfba9ba74f2ca9a05f5b6861ad5c1c3186b3fbde
= <=>
2023-08-21 16:14:59 -0500

    added shadow

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -2,7 +2,7 @@ export default function Links({ url, name, icon }) {
   return (
     <div className="flex justify-center items-center gap-x-2">
       <a href={url} target="_blank" rel="noreferrer">
-        <button className="bg-gradient-to-r from-main to-secondary transition duration-300 ease-in hover:-translate-y-1 hover:scale-110 rounded-xl text-accent2 p-3 text-xl font-bold flex flex-row justify-evenly items-center w-40">
+        <button className="bg-gradient-to-r from-main to-secondary transition duration-300 ease-in hover:-translate-y-1 hover:scale-110 rounded-xl text-accent2 p-3 text-xl font-bold flex flex-row justify-evenly items-center w-40 shadow-lg">
           {name} {icon}
         </button>
       </a>
