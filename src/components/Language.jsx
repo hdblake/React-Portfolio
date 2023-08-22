3adfaec930e5fcd4904c7c4d8e5bb8e56aab55fa
= <=>
2023-08-22 00:14:45 -0500

    completed

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -2,9 +2,21 @@ import { languages } from "../data/skills.js";
 
 export default function Language() {
   return (
-    <ul>
-      {languages &&
-        languages.map((skill) => <li key={skill.id}>{skill.name}</li>)}
-    </ul>
+    <div className="bg-accent2 border border-2 border-main rounded-xl p-4 shadow-lg">
+      <h2 className="text-2xl text-center font-bold text-accent1 underline decoration-2 decoration-secondary">
+        Programming Languages
+      </h2>
+      <ul>
+        {languages &&
+          languages.map((skill) => (
+            <li
+              key={skill.id}
+              className="text-2xl text-left text-accent1 my-2 flex flex-row items-center justify-evenly"
+            >
+              {skill.name} <img src={skill.icon} />
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 }
