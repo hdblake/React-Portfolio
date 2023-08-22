a9a03f4946a65a9061461c945fa0e8dc3be5f6eb
= <=>
2023-08-21 22:17:43 -0500

    created

---
+++ src/data/skills.js
@@ -0,0 +1,37 @@
+export const languages = [
+  {
+    id: 1,
+    name: "HTML/CSS",
+  },
+  {
+    id: 2,
+    name: "JavaScript",
+  },
+  {
+    id: 3,
+    name: "PHP",
+  },
+  {
+    id: 4,
+    name: "SQL",
+  },
+];
+
+export const frameworks = [
+  {
+    id: 1,
+    name: "Bootstrap",
+  },
+  {
+    id: 2,
+    name: "Tailwind",
+  },
+  {
+    id: 3,
+    name: "React",
+  },
+  {
+    id: 4,
+    name: "Material UI",
+  },
+];
