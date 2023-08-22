885aecdbce2742e0af9d6413682a009e4b622241
= <=>
2023-08-22 00:15:16 -0500

    completed

---
+++ src/components/Skills.jsx
@@ -0,0 +1,16 @@
+import Language from "./Language.jsx";
+import Framework from "./Framework.jsx";
+
+export default function Skills() {
+  return (
+    <section id="skills">
+      <h1 className="text-3xl md:text-4xl text-main underline decoration-2 decoration-accent1 mb-4 text-center font-bold">
+        Skills
+      </h1>
+      <div className="mt-6 flex flex-col gap-y-10 md:flex-row md:gap-x-20">
+        <Language />
+        <Framework />
+      </div>
+    </section>
+  );
+}
