59950a17f279e1309b98b41047444651ac2d23ec
= <=>
2023-08-22 00:15:05 -0500

    added skills link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="sticky top-0 bg-accent3 border-b-2 border-accent1 mb-8">
+    <header className="sticky top-0 bg-accent3 shadow-lg mb-8 z-10">
       <div className="flex flex-row items-center justify-between m-auto p-5 w-full lg:w-3/4">
         <img src="../images/logo.webp" alt="logo" width={190} height={59} />
         <nav>
@@ -11,8 +11,11 @@ export default function Header() {
             <li className="mr-5 text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
               <a href="#about">About</a>
             </li>
-            <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+            <li className="mr-5 text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
               <a href="#projects">Projects</a>
+            </li>
+            <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+              <a href="#skills">Skills</a>
             </li>
           </ul>
         </nav>
