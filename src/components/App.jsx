a1d79a746a9afaead7ca2a4b3e50792b59d7f91f
= <=>
2023-08-31 13:30:08 -0500

    added contact component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Home from "./Home.jsx";
 import About from "./About.jsx";
 import Projects from "./Projects.jsx";
 import Skills from "./Skills.jsx";
+import Contact from "./Contact.jsx";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <About />
         <Projects />
         <Skills />
+        <Contact />
       </main>
     </>
   );
