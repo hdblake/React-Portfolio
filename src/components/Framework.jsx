81c832a89b2532cf7c2af0533b28ac22762fe57c
= <=>
2023-08-31 13:29:57 -0500

    fixed margin

--- src/components/Framework.jsx
+++ src/components/Framework.jsx
@@ -2,7 +2,7 @@ import { frameworks } from "../data/skills.js";
 
 export default function Framework() {
   return (
-    <div className="bg-accent2 border border-2 border-main rounded-xl p-4 shadow-lg">
+    <div className="bg-accent2 border border-2 border-main rounded-xl p-4 shadow-lg m-2">
       <h2 className="text-2xl text-center font-bold text-accent1 underline decoration-2 decoration-secondary">
         Frameworks and Libraries
       </h2>
