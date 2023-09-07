8f43d5eaacd0e4f259a6ec48ae430a02a6c19572
= <=>
2023-09-07 16:54:13 -0500

    created footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-main p-4 w-full text-center">
+      <p className="text-accent2 font-bold text-lg">Hunter Blake 2023</p>
+    </footer>
+  );
+}
