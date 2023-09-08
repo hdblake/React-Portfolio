c1d4b2d8c8203fdcf6bcf9b1caaa8300e1546f08
= <=>
2023-09-08 00:47:19 -0500

    updated location

---
+++ public/data/skills.js
@@ -0,0 +1,45 @@
+export const languages = [
+  {
+    id: 1,
+    name: "HTML/CSS",
+    icon: "/icons/icons8-html-5-48.png",
+  },
+  {
+    id: 2,
+    name: "JavaScript",
+    icon: "/icons/icons8-javascript-48.png",
+  },
+  {
+    id: 3,
+    name: "PHP",
+    icon: "/icons/icons8-php-logo-48.png",
+  },
+  {
+    id: 4,
+    name: "SQL",
+    icon: "/icons/icons8-my-sql-48.png",
+  },
+];
+
+export const frameworks = [
+  {
+    id: 1,
+    name: "Bootstrap",
+    icon: "/icons/icons8-bootstrap-48.png",
+  },
+  {
+    id: 2,
+    name: "Tailwind",
+    icon: "/icons/icons8-tailwindcss-48.png",
+  },
+  {
+    id: 3,
+    name: "React",
+    icon: "/icons/icons8-react-48.png",
+  },
+  {
+    id: 4,
+    name: "Material UI",
+    icon: "/icons/icons8-material-ui-48.png",
+  },
+];
