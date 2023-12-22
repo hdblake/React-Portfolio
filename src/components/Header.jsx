07d09415ddaed236f1de192c1fc546a8c3fabcef
= <=>
2023-12-21 23:47:07 -0600

    updated responsive menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,43 @@
-import MenuIcon from "@mui/icons-material/Menu";
 import Logo from "../images/logo.webp";
+import { useState } from "react";
 
 export default function Header() {
-  function toggleMenu() {
-    document
-      .getElementsByClassName("navigation")[0]
-      .classList.toggle("responsive");
-  }
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <header className="bg-accent3 shadow-lg mb-8">
+    <header className="bg-accent3 shadow-lg mb-8 fixed top-0 w-full z-10">
       <div className="flex flex-row items-center justify-between m-auto p-5 w-full lg:w-3/4">
         <a href="#home">
           <img src={Logo} alt="logo" width={190} height={59} />
         </a>
         <nav>
-          <ul className="navigation flex flex-row">
-            <li>
-              <a href="#" onClick={toggleMenu}>
-                <MenuIcon style={{ color: "#f25c05", fontSize: "35px" }} />
-              </a>
-            </li>
-            <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
-              <a href="#about">About</a>
-            </li>
-            <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
-              <a href="#projects">Projects</a>
-            </li>
-            <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
-              <a href="#skills">Skills</a>
-            </li>
-            <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
+          <div className="flex flex-row items-center gap-x-2">
+            <div className="hamburger" onClick={toggleMenu}>
+              <div className={`line ${isOpen ? "open" : ""}`}></div>
+              <div className={`line ${isOpen ? "open" : ""}`}></div>
+              <div className={`line ${isOpen ? "open" : ""}`}></div>
+            </div>
+            <div className={`menu ${isOpen ? "open" : ""}`}>
+              <ul className="navigation flex flex-col md:flex-row gap-x-4">
+                <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+                  <a href="#about">About</a>
+                </li>
+                <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+                  <a href="#projects">Projects</a>
+                </li>
+                <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+                  <a href="#skills">Skills</a>
+                </li>
+                <li className="text-main text-xl lg:text-2xl font-bold hover:text-accent1 hover:underline hover:decoration-2 hover:decoration-secondary">
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
       </div>
     </header>
