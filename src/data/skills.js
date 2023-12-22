61b728f0c582a93b0f1379d33107d5be704a4033
= <=>
2023-12-21 21:06:46 -0600

    added databases and hosting sites

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,11 +1,17 @@
 import html from "../icons/icons8-html-5-48.png";
 import js from "../icons/icons8-javascript-48.png";
 import php from "../icons/icons8-php-logo-48.png";
-import sql from "../icons/icons8-my-sql-48.png";
+import mysql from "../icons/icons8-my-sql-48.png";
+import sql from "../icons/icons8-sql-48.png";
 import bootstrap from "../icons/icons8-bootstrap-48.png";
 import tailwind from "../icons/icons8-tailwindcss-48.png";
 import react from "../icons/icons8-react-48.png";
 import express from "../icons/icons8-express-js-48.png";
+import netlify from "../icons/icons8-netlify-48.png";
+import vercel from "../icons/icons8-triangle-48.png";
+import render from "../icons/icons8-hosting-48.png";
+import mssql from "../icons/icons8-microsoft-sql-server-48.png";
+import mongodb from "../icons/icons8-mongodb-48.png";
 
 export const languages = [
   {
@@ -50,5 +56,41 @@ export const frameworks = [
     id: 4,
     name: "Node/ExpressJS",
     icon: express,
+  },
+];
+
+export const databases = [
+  {
+    id: 1,
+    name: "MySQL",
+    icon: mysql,
+  },
+  {
+    id: 2,
+    name: "MSSQL Server",
+    icon: mssql,
+  },
+  {
+    id: 3,
+    name: "MongoDB",
+    icon: mongodb,
+  },
+];
+
+export const hostingSites = [
+  {
+    id: 1,
+    name: "Netlify",
+    icon: netlify,
+  },
+  {
+    id: 2,
+    name: "Vercel",
+    icon: vercel,
+  },
+  {
+    id: 3,
+    name: "Render",
+    icon: render,
   },
 ];
